1eb63b0c35e59288a8a3b3771482123a36ad569a
Manjunath Dhabe <67554477+manju-hub569@users.noreply.github.com>
2023-07-08 19:33:46 +0530

    DbConn

--- DB/conn.js
+++ DB/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-    mongoose.connect('mongodb://localhost:27017', {
+    mongoose.connect(process.env.mongourlprod, {
         useNewUrlParser:true,
         useUnifiedTopology:true,
     }).then(() => {
