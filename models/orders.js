9c6075e7a84a6d2d1c5d30bab9f6fba0505d793c
Manjunath Dhabe <67554477+manju-hub569@users.noreply.github.com>
2024-01-09 18:28:00 +0530

    models

---
+++ models/orders.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const orders = mongoose.Schema({
+
+    userid: {
+        type:mongoose.Schema.Types.ObjectId
+    },
+
+    name : {
+        type : String
+    },
+
+    title : {
+        type : String,
+        unique : true
+    },
+    price : {
+        type : String,
+        unique : true
+    },
+    description : {
+        type : String,
+        unique : true
+    },
+    category : {
+        type : String
+    },
+    image :{
+        type: String,
+        unique: false
+    }
+},{
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+    }
+});
+
+const Order = mongoose.model('order', orders);
+
+module.exports = Order;
---
+++ models/wishlist.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const List = mongoose.Schema({
+    userid : {
+        type:mongoose.Schema.Types.ObjectId
+    },
+    title : {
+        type : String,
+        unique : true
+    },
+    price : {
+        type : String,
+        unique : true
+    },
+    description : {
+        type : String,
+        unique : true
+    },
+    category : {
+        type : String
+    },
+    image :{
+        type: String,
+        unique: false
+    }
+});
+
+const WList = mongoose.model('wisjList', List);
+
+module.exports = WList;
