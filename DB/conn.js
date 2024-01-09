cf5ba1c719ec0e11e918955d8f7b57ccdb94c3e8
Manjunath Dhabe <67554477+manju-hub569@users.noreply.github.com>
2024-01-09 11:32:31 +0530

    changes

--- DB/conn.js
+++ DB/conn.js
@@ -10,24 +10,3 @@ const {config} = require('../utils/dbConfig');
         console.log(e);
     });
 
-    const connectToMongo = (mongoUrl) => {
-        return new Promise((resolve, reject) => {
-          MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(client.db("myapp"));
-            }
-          });
-        });
-      };
-
-      module.exports.dbcoll = async (colname) => {
-        const mongoUrl = config().mongoTest();
-        try {
-          const db = await connectToMongo(mongoUrl);
-          return db.collection(colname);
-        } catch (err) {
-          throw err;
-        }
-      };
